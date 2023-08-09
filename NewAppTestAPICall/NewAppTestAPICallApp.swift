46c1d0330d3f459ae7135bec5c386448e1d0eab4
Seravea <romainpoyard@gmail.com>
2023-08-09 10:07:35 +0200

    Location with good Info.plist informations, access to the user's location working

--- NewAppTestAPICall/NewAppTestAPICallApp.swift
+++ NewAppTestAPICall/NewAppTestAPICallApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NewAppTestAPICallApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WeatherCurrentLocationView()
         }
     }
 }
--- NewAppTestAPICall/View/WeatherCurrentLocationView.swift
+++ NewAppTestAPICall/View/WeatherCurrentLocationView.swift
@@ -15,30 +15,32 @@ struct WeatherCurrentLocationView: View {
     var body: some View {
         
         if locationManager.authorizationStatus == .authorizedWhenInUse {
-            ZStack(alignment: .topLeading) {
+           
                 
                 
-                VStack(spacing: 30) {
-                    Text("weathKitManager.date")
-                        .background(.yellow)
-                    
-                    Image(systemName: weathKitManager.symbol)
-                        .font(.system(size: 200))
-                    
-                    Text(weathKitManager.temp)
-                        .font(.system(size: 100))
-                        .bold()
-                        .fontDesign(.rounded)
-                    
-                    
-                }
+            VStack(spacing: 30) {
+                
+                Text("weathKitManager.date")
+                    .background(.yellow)
+                
+                Image(systemName: weathKitManager.symbol)
+                    .font(.system(size: 200))
+                
+                Text(weathKitManager.temp)
+                    .font(.system(size: 80))
+                    .bold()
+                    .fontDesign(.rounded)
+                
+            }
+            
+                
+                
+            
+        }else {
+           Text("We can't acces to your location")
                 .task {
                     await weathKitManager.getWeather(latitude: locationManager.latitude, longitude: locationManager.longitude)
                 }
-                
-            }
-        }else {
-           Text("We can't acces to your location")
         }
         
     }
