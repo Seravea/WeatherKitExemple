3cc8a06768acdfad62870d6a23d83778a463b616
Seravea <romainpoyard@gmail.com>
2023-08-09 09:15:02 +0200

    Initial Commit

---
+++ NewAppTestAPICall/NewAppTestAPICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAppTestAPICallApp.swift
+//  NewAppTestAPICall
+//
+//  Created by Romain Poyard on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewAppTestAPICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
